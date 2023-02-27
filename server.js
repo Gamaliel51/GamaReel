3a4bbb065252a5a19532713583e12cfb913e830f
Gamaliel51 <84592598+Gamaliel51@users.noreply.github.com>
2023-02-27 22:05:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -133,6 +133,6 @@ app.get("*", (req, res) => {
     res.send("Error")
 })
 
-app.listen(5000, () => {
-    console.log('listening on port 5000')
+app.listen(process.env.PORT, () => {
+    console.log('listening on port')
 })
